351acbe73c3c332470a67f34fdba2660d2f97330
Hoe <lkh_bear94@hotmail.com>
2022-08-30 21:12:19 +0800

    Update Terraform

---
+++ infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "Enter your AWS region in default value"
+  default     = "us-east-2"
+}
+
+variable "mlflow_artifact_bucket_name" {
+    description = "Enter your MLflow model artifact bucket name in default value"
+    default     = "mlops-project-mlflow-model-artifact"
+}
